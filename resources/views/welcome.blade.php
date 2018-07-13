871d669f9371715d58c18c9f8083c99b182a44da
jun hao <xephersteel@gmail.com>
2018-07-13 23:05:37 +0800

    Revert "renamed title"
    This reverts commit d76e11b97099d27199d8590c9eafdfa1d2676b1d.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,9 +84,9 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://www.google.com.sg">Google</a>
+                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://gmail.com">Gmail</a>
+                    <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
